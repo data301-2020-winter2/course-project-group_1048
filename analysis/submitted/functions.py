f5fa728e0de2579865c412aa1a3e7248ce1080ad
Riley Bolen <rileybolen123@gmail.com>
2021-03-17 18:38:18 -0700

    Fix merge error in savreen file and complete submission file

--- analysis/submitted/functions.py
+++ analysis/submitted/functions.py
@@ -13,6 +13,12 @@ def process_data_2(path):
     features_1 = features_1.dropna()
     return features_1
 
+def process_data_3(path):
+    dataset = pd.read_csv(path)
+    features_1 = dataset[['calculated_host_listings_count', 'reviews_per_month']]
+    features_1 = features_1.dropna()
+    return features_1
+
 def popular_locations(df):
     return df[df['neighbourhood_cleansed'].isin(['Downtown/Civic Center', 'Mission', 'Western Addition', 'South of Market', 'Castro/Upper Market', 'Bernal Heights', 'Haight Ashbury', 'Noe Valley', 'Outer Sunset', 'Nob Hill'])]
 
